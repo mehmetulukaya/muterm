82ee8f3b1c4c4dbfe3c42a55100775088bb17571
mehmet ulukaya <mehmetulukaya@users.noreply.github.com>
2025-10-01 16:54:31 +0300

    Update main.pas

--- main.pas
+++ main.pas
@@ -18,7 +18,7 @@ unit main;
 interface
 
 uses
-  Classes, SysUtils, FileUtil, SynHighlighterPas, SynEdit, SynCompletion,
+  Classes, SysUtils, LazFileUtils, SynHighlighterPas, SynEdit, SynCompletion,
   SynMemo, SynHighlighterAny,
   lNetComponents, uPSComponent, Forms, Controls, Graphics, Dialogs, StdCtrls,
   ExtCtrls, ComCtrls, Buttons, IniFiles, MuLibs, uPSR_std,
@@ -278,22 +278,6 @@ implementation
 {$R *.lfm}
 
 { TfrmMain }
-
-Function ExtractFileNameWithoutExt(Filenametouse:String):String;
-Begin
-  if ExtractFileExt(Filenametouse) <> '' then
-    Result := ExtractFilename(copy(Filenametouse,1,rpos(ExtractFileExt(Filenametouse),Filenametouse)-1))
-  else
-    Result := ExtractFilename(Filenametouse);
-End;
- 
-Function ExtractFilePathAndNameWithoutExt(Filenametouse:String):String;
-Begin
-  if ExtractFileExt(Filenametouse) <> '' then
-    Result := copy(Filenametouse,1,rpos(ExtractFileExt(Filenametouse),Filenametouse)-1)
-  else
-    Result := FilenametoUse;
-End;
 
  // --------------------------------------------------------------------
  // script functions
@@ -2411,5 +2395,6 @@ end;
 
 
 end.
+
 
 
